4611e389d91972d9676e3eca2efea81019ca075c
unhumain <titouan.pelou@esna.bzh>
2025-09-09 15:23:33 +0200

    Revert "le payload pastbin avance"
    This reverts commit 26b91f4d7070156000949bc4a3f914d463065d62.

--- backend/app/api/routes/api.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-# Déclarer un blueprint
-api_bp = Blueprint('api', __name__)
-
-@api_bp.route('/test', methods=['GET'])
-def test_route():
-    return {"message": "This is a test route!"}
--- frontend/src/views/about.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <h1>À propos</h1>
-  <p>Chacun son probleme</p>
-</template>
